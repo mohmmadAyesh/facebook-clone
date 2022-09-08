21fffac8a3ce1794f9dfc0c67f990ff40eeaa001
lol500q <mohmmadayesh22@gmail.com>
2022-09-08 20:46:45 +0300

    gg

--- HomePage/home.js
+++ HomePage/home.js
@@ -96,7 +96,7 @@ contactObject.forEach(obj=>{
     const card=`
     <div class="card">
     <div class="img-container">
-            <img src="../assests/${obj.image}" alt="">
+            <img src="./assests/${obj.image}" alt="">
             <span></span>
     </div>
     <span>${obj.title}</span>
@@ -114,7 +114,7 @@ console.log(comment);
 comment.addEventListener('keydown',e=>{
     if(e.key=='Enter'){
         const commentContent=`<div class="comments">
-        <img src="../assests/profile.jpg" alt="">
+        <img src="./assests/profile.jpg" alt="">
         <div class="text-container">
             <span class="title">Moh  Suhail</span>
             <span class="comments">${comment.value}</span>
@@ -254,7 +254,7 @@ const cardsFirstFromHTML=[
 const IconContainer=document.querySelector('.wrapper .icon-container .list');
 for(obj of cardsFirstFromHTML.reverse()){
     const list=`<li>
-                    <img src="../assests/${obj.image}" alt="">
+                    <img src="./assests/${obj.image}" alt="">
                     <p class="title">${obj.title}</p>
                 </li>
     `
@@ -264,7 +264,7 @@ let isMoreActivate=false;
 function InsertCards(){
     for(obj of cardsFirstScroll){
         const list=`<li class='addition'>
-                        <img src="../assests/${obj.image}" alt="">
+                        <img src="./assests/${obj.image}" alt="">
                         <p class="title">${obj.title}</p>
                     </li>
         `
@@ -347,7 +347,7 @@ function generateMessangerGuys(ul,objects){
     for(d of objects){
         const liContent=`<li>
         <div class="img-container">
-            <img src="../assests/${d.image}" alt="">
+            <img src="./assests/${d.image}" alt="">
             <span class='dot'></span>
             </div>
             <span class="title">${d.title}</span>
@@ -474,7 +474,7 @@ collectionBtn.addEventListener('click',()=>{
 const notificationGuys=[
     {
         title:'Hazem Allam Gamming',
-        content:` was live:<br>"Donate<img src='../assests/star.png' width="16px" height="16px"><br>
+        content:` was live:<br>"Donate<img src='./assests/star.png' width="16px" height="16px"><br>
         ::https:://streamlabs/hazem...`,
         image:'hazoom.jpg',
         sideImage:'<i data-visualcompletion="css-img" class="gneimcpu p9wrh9lq" style="height: 28px; width: 28px; background-image: url(&quot;https://static.xx.fbcdn.net/rsrc.php/v3/yy/r/6lGW-8_Fcno.png&quot;); background-position: 0px -638px; background-size: auto; background-repeat: no-repeat; display: inline-block;"></i>',
@@ -508,7 +508,7 @@ function generateNotificationGuys(){
     for(obj of notificationGuys){
     const li=`<li class="live">
                 <div class="img-container">
-                <img class="logo" src="../assests/${obj.image}" alt="">
+                <img class="logo" src="./assests/${obj.image}" alt="">
                 ${obj.sideImage}
                 </div>
 <span></span>
@@ -527,7 +527,7 @@ function generateNotificationGuys(){
     for(let i=0;i<4;i++){
         const li=`<li class="live">
                 <div class="img-container">
-                <img class="logo" src="../assests/${notificationGuys[0].image}" alt="">
+                <img class="logo" src="./assests/${notificationGuys[0].image}" alt="">
                 ${notificationGuys[0].sideImage}
                 </div>
 <span></span>
@@ -749,7 +749,7 @@ const otherContainer=document.querySelector('.menu-container .other ul');
         const li=`
         <li>
                                     <div class="img-container">
-                                    <img src="../assests/${obj.image}" alt="">
+                                    <img src="./assests/${obj.image}" alt="">
                                 </div>
                                     <div class="text-container">
                                         <span class="title">${obj.title}</span>
@@ -881,7 +881,7 @@ function generatePopUppost(ul,list){
         else{
             li=`
             <div class="li">
-                                <img src='../assests/report.png'>
+                                <img src='./assests/report.png'>
                                 <div class="text-container">
                                 <span class="title">${list[i].title}t</span>
                                 <span class="desc">${list[i].desc}</span>
@@ -926,7 +926,7 @@ function generatechatContainerContent(title,image){
                     <svg fill='#65676b' x="0px" y="0px" viewBox="0 0 17 16" height="20px" width="20px" class="b6ax4al1 m4pnbp5e aqweqrfb ahndzqod db0glzta"><g fill-rule="evenodd"><circle fill="none" cx="5.5" cy="5.5" r="1"></circle><circle fill="none" cx="11.5" cy="4.5" r="1"></circle><path d="M5.3 9c-.2.1-.4.4-.3.7.4 1.1 1.2 1.9 2.3 2.3h.2c.2 0 .4-.1.5-.3.1-.3 0-.5-.3-.6-.8-.4-1.4-1-1.7-1.8-.1-.2-.4-.4-.7-.3z" fill="none"></path><path d="M10.4 13.1c0 .9-.4 1.6-.9 2.2 4.1-1.1 6.8-5.1 6.5-9.3-.4.6-1 1.1-1.8 1.5-2 1-3.7 3.6-3.8 5.6z"></path><path d="M2.5 13.4c.1.8.6 1.6 1.3 2 .5.4 1.2.6 1.8.6h.6l.4-.1c1.6-.4 2.6-1.5 2.7-2.9.1-2.4 2.1-5.4 4.5-6.6 1.3-.7 1.9-1.6 1.9-2.8l-.2-.9c-.1-.8-.6-1.6-1.3-2-.7-.5-1.5-.7-2.4-.5L3.6 1.5C1.9 1.8.7 3.4 1 5.2l1.5 8.2zm9-8.9c.6 0 1 .4 1 1s-.4 1-1 1-1-.4-1-1 .4-1 1-1zm-3.57 6.662c.3.1.4.4.3.6-.1.3-.3.4-.5.4h-.2c-1-.4-1.9-1.3-2.3-2.3-.1-.3.1-.6.3-.7.3-.1.5 0 .6.3.4.8 1 1.4 1.8 1.7zM5.5 5.5c.6 0 1 .4 1 1s-.4 1-1 1-1-.4-1-1 .4-1 1-1z" fill-rule="nonzero"></path></g></svg>
                     <svg fill='#65676b' x="0px" y="0px" viewBox="0 0 16 16" height="20px" width="20px" class="b6ax4al1 m4pnbp5e aqweqrfb ahndzqod db0glzta"><path d="M.783 12.705c.4.8 1.017 1.206 1.817 1.606 0 0 1.3.594 2.5.694 1 .1 1.9.1 2.9.1s1.9 0 2.9-.1 1.679-.294 2.479-.694c.8-.4 1.157-.906 1.557-1.706.018 0 .4-1.405.5-2.505.1-1.2.1-3 0-4.3-.1-1.1-.073-1.976-.473-2.676-.4-.8-.863-1.408-1.763-1.808-.6-.3-1.2-.3-2.4-.4-1.8-.1-3.8-.1-5.7 0-1 .1-1.7.1-2.5.5s-1.417 1.1-1.817 1.9c0 0-.4 1.484-.5 2.584-.1 1.2-.1 3 0 4.3.1 1 .2 1.705.5 2.505zm10.498-8.274h2.3c.4 0 .769.196.769.696 0 .5-.247.68-.747.68l-1.793.02.022 1.412 1.252-.02c.4 0 .835.204.835.704s-.442.696-.842.696H11.82l-.045 2.139c0 .4-.194.8-.694.8-.5 0-.7-.3-.7-.8l-.031-5.631c0-.4.43-.696.93-.696zm-3.285.771c0-.5.3-.8.8-.8s.8.3.8.8l-.037 5.579c0 .4-.3.8-.8.8s-.8-.4-.8-.8l.037-5.579zm-3.192-.825c.7 0 1.307.183 1.807.683.3.3.4.7.1 1-.2.4-.7.4-1 .1-.2-.1-.5-.3-.9-.3-1 0-2.011.84-2.011 2.14 0 1.3.795 2.227 1.695 2.227.4 0 .805.073 1.105-.127V8.6c0-.4.3-.8.8-.8s.8.3.8.8v1.8c0 .2.037.071-.063.271-.7.7-1.57.991-2.47.991C2.868 11.662 1.3 10.2 1.3 8s1.704-3.623 3.504-3.623z" fill-rule="nonzero"></path></svg>
                     <div class="inputField">
-                      <img src="../assests/smile.jpg" alt="">
+                      <img src="./assests/smile.jpg" alt="">
                         <input type="text" placeholder="Aa">
                     </div>
                     <svg fill='#65676b' viewBox="0 0 16 16" height="20" width="20" class="db0glzta"><path d="M16,9.1c0-0.8-0.3-1.1-0.6-1.3c0.2-0.3,0.3-0.7,0.3-1.2c0-1-0.8-1.7-2.1-1.7h-3.1c0.1-0.5,0.2-1.3,0.2-1.8 c0-1.1-0.3-2.4-1.2-3C9.3,0.1,9,0,8.7,0C8.1,0,7.7,0.2,7.6,0.4C7.5,0.5,7.5,0.6,7.5,0.7L7.6,3c0,0.2,0,0.4-0.1,0.5L5.7,6.6 c0,0-0.1,0.1-0.1,0.1l0,0l0,0L5.3,6.8C5.1,7,5,7.2,5,7.4v6.1c0,0.2,0.1,0.4,0.2,0.5c0.1,0.1,1,1,2,1h5.2c0.9,0,1.4-0.3,1.8-0.9 c0.3-0.5,0.2-1,0.1-1.4c0.5-0.2,0.9-0.5,1.1-1.2c0.1-0.4,0-0.8-0.2-1C15.6,10.3,16,9.9,16,9.1z"></path><path d="M3.3,6H0.7C0.3,6,0,6.3,0,6.7v8.5C0,15.7,0.3,16,0.7,16h2.5C3.7,16,4,15.7,4,15.3V6.7C4,6.3,3.7,6,3.3,6z"></path></svg>
